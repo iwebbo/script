c043c19bb944163218ed4cf7b85e1e767af9e99b
A&ECoding <l.kieran95@gmail.com>
2025-02-27 17:11:23 +0100

    Update check_sso.py

--- Python/check_sso.py
+++ Python/check_sso.py
@@ -35,7 +35,7 @@ def test_authenticated_page(cookies):
     conn = http.client.HTTPSConnection(GRAFANA_HOST)
     headers = {
         "Cookie": cookies,
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.69"
     }
 
     conn.request("GET", GRAFANA_TEST_PAGE, headers=headers)
